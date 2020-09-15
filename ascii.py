6693de818e1468d306961f84e1001affac05306b
Daniel <daniel.sdm.github@gmail.com>
2020-09-15 00:58:07 -0300

    ✏️ ASCII Arts

---
+++ ascii.py
@@ -0,0 +1,111 @@
+from rich import print
+
+windows = """[bold magenta]
+                 __
+            ,-~¨^  ^¨-,           _,
+           /          / ;^-._...,¨/
+          /          / /         /
+         /          / /         /
+        /          / /         /
+       /,.-:''-,_ / /         /
+       _,.-:--._ ^ ^:-._ __../
+     /^         / /¨:.._¨__.;
+    /          / /      ^  /
+   /          / /         /
+  /          / /         /
+ /_,.--:^-._/ /         /
+^            ^¨¨-.___.:^ 
+[/bold magenta]
+"""
+
+linux = """[bold magenta]
+
+                 .88888888:.
+                88888888.88888.
+              .8888888888888888.
+              888888888888888888
+              88' _`88'_  `88888
+              88 88 88 88  88888
+              88_88_::_88_:88888
+              88:::,::,:::::8888
+              88`:::::::::'`8888
+             .88  `::::'    8:88.
+            8888            `8:888.
+          .8888'             `888888.
+         .8888:..  .::.  ...:'8888888:.
+        .8888.'     :'     `'::`88:88888
+       .8888        '         `.888:8888.
+      888:8         .           888:88888
+    .888:88        .:           888:88888:
+    8888888.       ::           88:888888
+    `.::.888.      ::          .88888888
+   .::::::.888.    ::         :::`8888'.:.
+  ::::::::::.888   '         .::::::::::::
+  ::::::::::::.8    '      .:8::::::::::::.
+ .::::::::::::::.        .:888:::::::::::::
+ :::::::::::::::88:.__..:88888:::::::::::'
+  `'.:::::::::::88888888888.88:::::::::'
+        `':::_:' -- '' -'-' `':_::::'`
+
+[/bold magenta]
+"""
+
+macos = """
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdyo/:yMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMdo/:::::hMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMm+:::::::+MMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMh/:::::::/mMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMd::::::::sNMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMM+::::::odMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMM::/+ohNMMMMMMMMMMMMMMMMMM
+MMMMMMMMNmhso++++oshdmMMMNNNNmdyso+///+osydmMMMMMM
+MMMMMNds//::::::::::://osys+//:::::::::::::/+yNMMM
+MMMMh+::::::::::::::::::::::::::::::::::::::::/yNM
+MMNo::::::::::::::::::::::::::::::::::::::::::ohMM
+Mm+:::::::::::::::::::::::::::::::::::::::::+dMMMM
+M+:::::::::::::::::::::::::::::::::::::::::yNMMMMM
+h:::::::::::::::::::::::::::::::::::::::::sMMMMMMM
++:::::::::::::::::::::::::::::::::::::::::NMMMMMMM
+:::::::::::::::::::::::::::::::::::::::::/MMMMMMMM
+:::::::::::::::::::::::::::::::::::::::::/MMMMMMMM
+/:::::::::::::::::::::::::::::::::::::::::dMMMMMMM
+o:::::::::::::::::::::::::::::::::::::::::+NMMMMMM
+d::::::::::::::::::::::::::::::::::::::::::+mMMMMM
+M+::::::::::::::::::::::::::::::::::::::::::/smMMM
+Mm::::::::::::::::::::::::::::::::::::::::::::/ohm
+MMy::::::::::::::::::::::::::::::::::::::::::::::s
+MMMs::::::::::::::::::::::::::::::::::::::::::::+N
+MMMMs::::::::::::::::::::::::::::::::::::::::::+NM
+MMMMMy::::::::::::::::::::::::::::::::::::::::oNMM
+MMMMMMd/:::::::::::::::::::::::::::::::::::::yMMMM
+MMMMMMMNs::::::::::::::::::::::::::::::::::+mMMMMM
+MMMMMMMMMmo:::::::::::::/+++/::::::::::::/hMMMMMMM
+MMMMMMMMMMMNyo/::/+sydNMMMMMMNmhso//:/+sdMMMMMMMMM
+
+"""
+
+unknow = """
+     .... NO! ...                  ... MNO! ...
+   ..... MNO!! ...................... MNNOO! ...
+ ..... MMNO! ......................... MNNOO!! .
+..... MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!! .
+ ... !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO! ....
+    ...... ! MMMMMMMMMMMMMPPPPOOOOIII! ! ...
+   ........ MMMMMMMMMMMMPPPPPOOOOOOII!! .....
+   ........ MMMMMOOOOOOPPPPPPPPOOOOMII! ...
+    ....... MMMMM..    OPPMMP    .,OMI! ....
+     ...... MMMM::   o.,OPMP,.o   ::I!! ...
+         .... NNM:::.,,OOPM!P,.::::!! ....
+          .. MMNNNNNOOOOPMO!!IIPPO!!O! .....
+         ... MMMMMNNNNOO:!!:!!IPPPPOO! ....
+           .. MMMMMNNOOMMNNIIIPPPOO!! ......
+          ...... MMMONNMMNNNIIIOO!..........
+       ....... MN MOMMMNNNIIIIIO! OO ..........
+    ......... MNO! IiiiiiiiiiiiI OOOO ...........
+  ...... NNN.MNO! . O!!!!!!!!!O . OONO NO! ........
+   .... MNNNNNO! ...OOOOOOOOOOO .  MMNNON!........
+   ...... MNNNNO! .. PPPPPPPPP .. MMNON!........
+      ...... OO! ................. ON! .......
+         ................................
+
+"""
